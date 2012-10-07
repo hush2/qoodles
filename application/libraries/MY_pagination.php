ad3e8fdc1e6fc3ac2cd1f490f5360248225c72b4
hush2 <hushywushy@gmail.com>
2012-10-07 09:06:19 +0800

    auto-commit

--- application/libraries/MY_pagination.php
+++ application/libraries/MY_pagination.php
@@ -3,6 +3,10 @@
 
 class MY_Pagination extends CI_Pagination
 {
+    public function __construct()
+    {
+        die('wtf');
+    }
     public function create($url, $total_rows, $per_page, $uri_segment=4)
     {
         $this->initialize(array(
